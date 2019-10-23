9804470a90d41f4a729213b41d1556b1171d77ab
joe zhang <joe.zhang@ringcentral.com>
2019-10-23 09:52:39 +0800

    use logstash

--- app/config/application.rb
+++ app/config/application.rb
@@ -11,6 +11,10 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.lograge.enabled = true
+    config.lograge.formatter = Lograge::Formatters::Logstash.new
+    config.lograge.logger = LogStashLogger.new(type: :udp, host: 'logstash01.lab.nordigy.ru', port: 1888)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
